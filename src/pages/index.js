e8a622dcbaf768fc8af8fcc180f66711d1102849
yerastovanton <tonyyerastov@gmail.com>
2025-01-03 12:12:21 +0300

    new file:   .gitignore 	new file:   .prettierrc 	new file:   package-lock.json 	new file:   package.json 	new file:   src/assets/styles/fonts.module.scss 	new file:   src/assets/styles/mixins.module.scss 	new file:   src/assets/styles/normalize.module.scss 	new file:   src/assets/styles/styles.module.scss 	new file:   src/assets/styles/theme.module.scss 	new file:   src/assets/styles/vars.module.scss 	new file:   src/components/Button/Button.module.scss 	new file:   src/components/Button/Button.pug 	new file:   src/components/Checkbox/Checkbox.module.scss 	new file:   src/components/Checkbox/Checkbox.pug 	new file:   src/components/ColorItem/ColorItem.module.scss 	new file:   src/components/ColorItem/ColorItem.pug 	new file:   src/components/LikeButton/LikeButton.module.scss 	new file:   src/components/LikeButton/LikeButton.pug 	new file:   src/components/RadioButton/RadioButton.module.scss 	new file:   src/components/RadioButton/RadioButton.pug 	new file:   src/components/RateButton/RateButton.module.scss 	new file:   src/components/RateButton/RateButton.pug 	new file:   src/components/Text/Text.module.scss 	new file:   src/components/Text/Text.pug 	new file:   src/components/TextField/TextField.module.scss 	new file:   src/components/TextField/TextField.pug 	new file:   src/components/Toggle/Toggle.module.scss 	new file:   src/components/Toggle/Toggle.pug 	new file:   src/components/index.module.scss 	new file:   src/components/mixins/errorMixin.pug 	new file:   src/pages/index.js 	new file:   src/pages/index.pug 	new file:   src/pages/website/temple/temple.js 	new file:   src/pages/website/temple/temple.module.scss 	new file:   src/pages/website/temple/temple.pug 	new file:   src/pages/website/temple2/temple2.js 	new file:   src/pages/website/temple2/temple2.module.scss 	new file:   src/pages/website/temple2/temple2.pug 	new file:   webpack.config.js

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import * as c from './website/temple/temple.js';
+import * as d from './website/temple2/temple2.js';
+
+import '@styles/styles.module.scss';
+
+console.log(c, d);
---
+++ webpack.config.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = (env) => {
+    const isDev = env.mode === 'development';
+    const absoluteDirPath = path.resolve(__dirname, './src/pages/');
+    const entriesSettings = path.resolve(__dirname, './src/pages/index.js');
+
+    const getHtmlWebpackPluginSetting = (dir) => {
+        const htmlWebpackPluginSetting = [];
+
+        const getAllFiles = (dir) =>
+            fs.readdirSync(dir).reduce((files, file) => {
+              const name = path.join(dir, file);
+              const isDirectory = fs.statSync(name).isDirectory();
+              return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
+            }, []);
+
+        for (const file of getAllFiles(dir)) {
+            // if (path.extname(file) == '.js') {
+                //     entriesAndPages.entries.push(
+                //         `${file}`
+                //     );
+                // };
+            if (path.extname(file) == '.pug') {
+                const deleteExt = path.basename(`${file}`, '.pug');
+                htmlWebpackPluginSetting.push(
+                    new HtmlWebpackPlugin({
+                        template: file,
+                        filename: `${deleteExt}.html`,
+                    })
+                );
+            };
+        };
+
+        return htmlWebpackPluginSetting;
+    };
+
+    const htmlWebpackPluginSetting = getHtmlWebpackPluginSetting(absoluteDirPath);
+
+    const cssLoaderWithModules = {
+        loader: 'css-loader',
+        options: {
+            modules: {
+            localIdentName: isDev ? '[local]' : '[hash:base64:8]',
+            },
+        },
+    }; 
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? MiniCssExtractPlugin.loader : 'style-loader', 
+            cssLoaderWithModules, 
+            'sass-loader',
+        ],
+    };
+    const assetsLoader = {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    };
+    const pugLoader = {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+    };
+
+return {
+    mode: env.mode || 'development',
+        entry: entriesSettings,
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].[contenthash].js',
+            clean: true,
+        },
+        resolve: {
+            alias: {
+                "@src": path.resolve(__dirname, "src"),
+                "@assets": path.resolve(__dirname, "src/assets"),
+                "@components": path.resolve(__dirname, "src/components"),
+                "@pages": path.resolve(__dirname, "src/pages"),
+                "@styles": path.resolve(__dirname, "src/assets/styles"),
+            },
+        },
+        module: {
+            rules: [
+                pugLoader,
+                assetsLoader,
+                cssLoader,
+            ],
+        },
+        plugins: [
+            isDev && new webpack.ProgressPlugin(),
+            isDev && new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css',
+            }),
+            ...htmlWebpackPluginSetting,
+        ],
+        devtool: isDev ? false : 'inline-source-map',
+        devServer: isDev ? {
+            port: env.port || 5000,
+            open: true,
+        } : undefined,
+    };
+};
